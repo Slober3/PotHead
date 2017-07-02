d03988aa88e53fe5841ccbf0abc3f6c0cb01db28
QuintenD <quintendesmyter@hotmail.com>
2017-07-02 19:56:55 +0200

    Update SQL query
    Removing empty input fields from list

--- Website+Backend/inputView.php
+++ Website+Backend/inputView.php
@@ -112,7 +112,7 @@ class TableRows extends RecursiveIteratorIterator {
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM ( SELECT id, time, ip, input, port FROM loginput ORDER BY id DESC LIMIT 30 ) sub ORDER BY id ASC"); 
+    $stmt = $conn->prepare("SELECT * FROM ( SELECT id, time, ip, input, port FROM loginput WHERE input IS NOT NULL AND input <> '\r\n' ORDER BY id DESC LIMIT 30 ) sub ORDER BY id ASC"); 
     $stmt->execute();
 
     // set the resulting array to associative
