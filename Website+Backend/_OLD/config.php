521418ff58ddbf9e98dbc5d675eb4881b0476e88
QuintenD <quintendesmyter@hotmail.com>
2017-07-03 13:58:56 +0200

    Removed _OLD files
    Removed older legacy files.

--- Website+Backend/_OLD/config.php
+++
@@ -1,9 +0,0 @@
-<?//comment out this line if you do not need an .txt logfile
-$logfile = "PotHeadLog.txt";
-
-//comment out this line if you do not need an MySQL log
-$servername = "localhost";
-$username = "USERNAME";
-$password = "PASSWORD";
-$dbname = "DBNAME";
-?>
