92e23be49830dfe8aa123a31f5fa61de2470a4ca
Slober3 <quintendesmyter@hotmail.com>
2017-07-05 22:16:34 +0200

    Improved  JS code
    Improved JS code futher improvements will follow soon

--- Website+Backend/js/scriptv2.js
+++ Website+Backend/js/scriptv2.js
@@ -1,4 +1,5 @@
-$.getJSON( "http://7ol.eu/api/api.php/connection/last/10", function( data ) {
+function getLast(number){
+$.getJSON( "http://7ol.eu/api/api.php/connection/last/"+number, function( data ) {
   var items = [];
   $.each( data, function( key, val ) {
     items.push( "<tr>"+ "<td>"+val.id+"</td>" + "<td>"+val.time+"</td>"+ "<td>"+"<a href='http://www.ipvoid.com/scan/"+val.ip+"/'>"+val.ip+"</a>"+"</td>"+ "<td>"+val.port+"</td>"+ "<td>"+val.portHacker+"</td>" + "</tr>" );
@@ -8,7 +9,9 @@ $.getJSON( "http://7ol.eu/api/api.php/connection/last/10", function( data ) {
     html: items.join( "" )
   }).appendTo( "table" );
 });
+}
 
+getLast(10);
 
 $("#ipInput").keyup(function(event){
     if(event.keyCode === 13){
@@ -22,16 +25,8 @@ $( "tbody" ).remove();
 
 if (!$("#ipInput").val()) {
 $( "#hiddenth" ).addClass( "hide" );
-$.getJSON( "http://7ol.eu/api/api.php/connection/last/10", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<tr>"+ "<td>"+val.id+"</td>" + "<td>"+val.time+"</td>"+ "<td>"+"<a href='http://www.ipvoid.com/scan/"+val.ip+"/'>"+val.ip+"</a>"+"</td>"+ "<td>"+val.port+"</td>"+ "<td>"+val.portHacker+"</td>" + "</tr>" );
-  });
- 
-  $( "<tbody/>", {
-    html: items.join( "" )
-  }).appendTo( "table" );
-});} else { 
+getLast(10);
+} else { 
 $.getJSON( "http://7ol.eu/api/api.php/cinput/ip/"+$("#ipInput").val(), function( data ) {
   var items = [];
   $.each( data, function( key, val ) {
